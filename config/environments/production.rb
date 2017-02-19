3273729faaf34e3aa619bf24bf9aac4e79762df7
Russell Schmidt <russell.schmidt@me.com>
2017-02-19 15:25:51 -0800

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "cc3_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Configure ActionMailer per devise --- host needs to match actual host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
