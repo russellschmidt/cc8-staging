2d972206725d7007faacc6fea820a7f71585029f
Russell Schmidt <russell.schmidt@me.com>
2017-03-06 11:52:47 -0800

    got tabs and top half of project page up

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,14 +1,5 @@
 module ProjectsHelper
   def return_hero_image_url(partner_id)
-    # 'https://s3.amazonaws.com/climatecents3/HighResProject/mend-project-hero-1024x680.jpg'
-    # images = Image.where(partner_id: partner_id)
-    # images.each do |image|
-    #   if /(hero)/.match(image.aws_url)
-    #     return image.aws_url
-    #   else
-    #     return nil
-    #   end
-    # end
     images = Image.where(partner_id: partner_id)
     images.each do |image|
       if /(hero)/.match(image.aws_url)
@@ -18,14 +9,7 @@ module ProjectsHelper
   end
 
   def return_logo_image_url(partner_id)
-    images = Image.where(partner_id: partner_id)
-    images.each do |image|
-      if image.logo
-        return image.aws_url
-      else
-        return nil
-      end
-    end
+    Image.where(partner_id: partner_id, logo: true).first.aws_url
   end
 
   def get_partner(partner_id)
