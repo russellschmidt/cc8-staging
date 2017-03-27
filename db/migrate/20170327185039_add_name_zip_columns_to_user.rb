a9e04be5f1f574152051aba8b479230041165df2
Russell Schmidt <russell.schmidt@me.com>
2017-03-27 11:51:53 -0700

    add name zip to User model

---
+++ db/migrate/20170327185039_add_name_zip_columns_to_user.rb
@@ -0,0 +1,6 @@
+class AddNameZipColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :zip, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327020747) do
+ActiveRecord::Schema.define(version: 20170327185039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,8 @@ ActiveRecord::Schema.define(version: 20170327020747) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
+    t.string   "name"
+    t.string   "zip"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
