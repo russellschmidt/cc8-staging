2b4b284426d22c363ee46760a775877666082abc
Russell Schmidt <russell.schmidt@me.com>
2017-03-26 19:24:25 -0700

    got slugs working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,16 @@
 class ProjectsController < ApplicationController
 
   def show
-    @project = Project.find(params[:id])
+    @project = Project.friendly.find(params[:id])
     @partner = find_partner(@project)
   end
 
   private
     def project_params
-      params.require(:project).permit(:name, :blurb, :description, :partner_id)
+      params.require(:project).permit(:name, :blurb, :description, :partner_id, :slug, :videoUrl)
     end
 
     def find_partner(project)
-      Partner.friendly.find(project.partner_id)
+      Partner.find(project.partner_id)
     end
 end
