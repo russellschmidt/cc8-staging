681c8a47d42f19eb5052aa28f49bfb3863d3da36
Russell Schmidt <russell.schmidt@me.com>
2017-03-28 12:11:18 -0700

    almost done w confirmation page styling first pass

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -3,13 +3,9 @@ module ChargesHelper
     Project.friendly.find(project_id)
   end
 
-  def get_partner(project_id)
+  def get_partner_from_project_id(project_id)
     project = get_project(project_id)
     Partner.find(project.partner_id)
-  end
-
-  def return_logo_image_url(partner_id)
-    Image.where(partner_id: partner_id, logo: true).first.aws_url
   end
 
 end
