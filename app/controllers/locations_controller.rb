40bc2dd2b5cdee531fd3c0b4a1eba04b10285675
Russell Schmidt <russell.schmidt@me.com>
2017-04-04 15:32:06 -0700

    complete locations admin save for authentication

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,9 +2,31 @@ class LocationsController < ApplicationController
   layout 'administration'
 
   def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.create(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      flash[:error] = "Error saving the location to our database"
+      redirect_to locations_path
+    end
   end
 
   def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to location_path(@location)
+    else
+      flash[:error] = "Error updating the location in our database"
+      redirect_to locations_path
+    end
   end
 
   def index
@@ -15,4 +37,10 @@ class LocationsController < ApplicationController
     @location = Location.find(params[:id])
     @partners = Partner.where(location_id: @location.id)
   end
+
+  private
+
+    def location_params
+      params.require(:location).permit(:name)
+    end
 end
