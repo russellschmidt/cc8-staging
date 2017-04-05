753103480fda7e35eca49ecc30f84ada1f6fdf98
Russell Schmidt <russell.schmidt@me.com>
2017-04-05 01:48:11 -0700

    add default images to three paperclip Project images

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ApplicationRecord
 
   has_many :campaigns
 
-  has_attached_file :logo, styles: { standard: "80>x80" }
-  has_attached_file :hero_image, styles: { large: "800>x664" }, default_url: ""
-  has_attached_file :small_image, styles: { large: "350x200" }
+  has_attached_file :logo, styles: { standard: "80>x80" }, default_url: "https://s3.amazonaws.com/climatecents3/ClimateCents_2017Site_01_ProductionFile_20170114/logo_climatecents_med_white.png"
+  has_attached_file :hero_image, styles: { large: "800>x664" }, default_url: "https://s3.amazonaws.com/climatecents3/ClimateCents_2017Site_01_ProductionFile_20170114/277H-compressor.png"
+  has_attached_file :small_image, styles: { large: "350x200" }, default_url: "https://s3.amazonaws.com/climatecents3/ClimateCents_2017Site_01_ProductionFile_20170114/microcrafts570.jpg"
 end
