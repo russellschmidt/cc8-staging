3093b07714e825573a1e2dd84061c94e0f648749
Russell Schmidt <russell.schmidt@me.com>
2017-04-04 17:00:23 -0700

    add most of partner admin in

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,2 +1,50 @@
 class PartnersController < ApplicationController
+  layout 'administration'
+
+  before_filter :find_partner, only: [:edit, :update, :show]
+
+  def new
+    @partner = Partner.new
+  end
+
+  def create
+    @partner = Partner.create(partner_params)
+    if @partner.save
+      redirect_to partner_path(@partner)
+    else
+      flash[:error] = "Error saving the location to our database"
+      redirect_to partners_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @partner.update(partner_params)
+      redirect_to partner_path(@partner)
+    else
+      flash[:error] = "Error updating the location in our database"
+      redirect_to partners_path
+    end
+  end
+
+  def index
+    @partners = Partner.all
+  end
+
+  def show
+    @projects = Project.where(partner_id: @partner.id)
+  end
+
+  private
+  # private
+    def find_partner
+      @partner = Partner.find(params[:id])
+    end
+
+    def partner_params
+      params.require(:location).permit(:name, :blurb, :location_id, :description, :news, :volunteer)
+    end
+  # end private
 end
--- app/helpers/partners_helper.rb
+++ app/helpers/partners_helper.rb
@@ -1,2 +1,13 @@
 module PartnersHelper
+  def active_campaigns_by_project(project)
+    active_campaigns = []
+    campaigns = campaigns_by_project(project)
+
+    campaigns.each do |campaign|
+      if campaign.end_date > Time.now
+        active_campaigns << campaign
+      end
+    end
+    active_campaigns.count
+  end
 end
