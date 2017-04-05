a7772218b3ad278774984d4145b91f8ed7539223
Russell Schmidt <russell.schmidt@me.com>
2017-04-05 12:35:47 -0700

    index 5/6ths done for campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,34 @@
 class CampaignsController < ApplicationController
+  layout "administration"
+  before_filter :find_campaign, only: [:show, :edit, :create]
+
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  # start private
+    def campaign_params
+    end
+
+    def find_campaign
+      @campaign = Campaign.find(params[:id])
+    end
+
+  # end private
 end
--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,9 @@
 module CampaignsHelper
+  def campaign_active_inactive(campaign)
+    if campaign.end_date > Time.now
+      "active"
+    else
+      "inactive"
+    end
+  end
 end
