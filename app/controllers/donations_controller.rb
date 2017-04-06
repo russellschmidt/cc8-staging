f364a5d10b201c4f3702e5cbe2754248a93bb010
Russell Schmidt <russell.schmidt@me.com>
2017-04-05 20:15:37 -0700

    donations index and controller in

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -38,7 +38,9 @@ class DonationsController < ApplicationController
   private
   # start private
     def donation_params
-      params.require(:donation).permit(:)
+      params.require(:donation).permit(:user_id, :campaign_id, :stripe_email,
+        :stripe_customer_id, :total_in_cents, :campaign_donation_in_cents,
+        :tip_in_cents, :name, :address, :city, :state, :zip, :country, :countryCode)
     end
 
     def find_donation
