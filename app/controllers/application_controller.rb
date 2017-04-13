0374ec0d0c12d53c380ecb1bd6eba7f999c6876f
Russell Schmidt <russell.schmidt@me.com>
2017-04-13 10:51:51 -0700

    add default sign in path to applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   @content = "Support local, vetted projects to fight climate change in your community"
+
+  def after_sign_in_path_for(resource)
+    admin_index_path
+  end
 end
